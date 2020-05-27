7723592b249bac80a304847b0bf8afeb2a333e15
w4ld0j <nada_p1@hotmail.com>
2020-05-26 23:18:08 -0500

    Modificacion a Contacto.
    Imagen y sonido de fondo.

--- app/src/main/java/com/example/privapp/ui/send/SendFragment.java
+++ app/src/main/java/com/example/privapp/ui/send/SendFragment.java
@@ -1,5 +1,6 @@
 package com.example.privapp.ui.send;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import com.example.privapp.R;
 public class SendFragment extends Fragment {
 
     private SendViewModel sendViewModel;
+    private MediaPlayer mediaPlayer;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -30,6 +32,37 @@ public class SendFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        //View v = inflater.inflate(R.layout.fragment_slideshow, container, false);
+        //ImageView imageView = (ImageView) v.findViewById(R.id.imageView3);
+
+        mediaPlayer = MediaPlayer.create(getActivity(), R.raw.crude_mobile_ringtone_converter);
+        mediaPlayer.start();
+
+        /*Glide.with(this)
+                .asGif()
+                .load(R.raw.gavilan)
+                .into(imageView);*/
+
         return root;
+    }
+    /*@Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+        // Make sure that we are currently visible
+        if (this.isVisible()) {
+            // If we are becoming invisible, then...
+            if (!isVisibleToUser) {
+                mediaPlayer.stop();
+            }
+            else {
+                // do what you like
+            }
+        }
+    }*/
+    @Override
+    public void onPause() {
+        super.onPause();
+        mediaPlayer.pause();
     }
 }
